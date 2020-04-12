abf3452ad61a0d896715e2151d78e3f23dbbbf34
Robson Maia <robsonmvieira@gmail.com>
2020-04-12 14:39:57 -0300

    chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  transform: { '.+\\.ts$': 'ts-jest' }
+}
