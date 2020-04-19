de73bd6e9f0bb81fca2e4ffc4bf4d6a6e3af2313
Robson Maia <robsonmvieira@gmail.com>
2020-04-19 16:26:29 -0300

    feat: add decoretor properties in post model

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,9 +1,25 @@
-import { Entity } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm'
 
 @Entity('posts')
 export class Post {
-  public id: string;
-  public title: string;
-  public subtitle: string;
-  public content: string
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column('boolean')
+  draw: boolean
+
+  @Column()
+  subtitle: string;
+
+  @Column()
+  content: string
+
+  @CreateDateColumn()
+  cretated_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
 }
