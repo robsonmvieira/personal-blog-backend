b9f423ff48693579a01cc3dcf3e10ed4c0854842
Robson Maia <robsonmvieira@gmail.com>
2020-04-21 20:00:58 -0300

    feat: add Tag router resources url in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import postController from './controllers/postController'
+import tagController from './controllers/tagController'
 import 'reflect-metadata'
 import './database'
 const app = express()
@@ -7,5 +8,6 @@ const app = express()
 app.use(express.json())
 
 app.use('/posts', postController)
+app.use('/tags', tagController)
 
 app.listen(8000, () => console.log('app server running : http://localhost:8000'))
