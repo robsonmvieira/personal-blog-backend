a1965e5a4b0583f5c5603a0d6884364cd6b4dbfa
Robson Maia <robsonmvieira@gmail.com>
2020-04-21 20:08:30 -0300

    feat: add cors lib to access from front-end

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 import postController from './controllers/postController'
 import tagController from './controllers/tagController'
+import categoryController from './controllers/categoryController'
 import 'reflect-metadata'
 import './database'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/posts', postController)
 app.use('/tags', tagController)
+app.use('/categories', categoryController)
 
 app.listen(8000, () => console.log('app server running : http://localhost:8000'))
