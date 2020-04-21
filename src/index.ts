21963ada82621bc916aee8c72701015753c6f21c
Robson Maia <robsonmvieira@gmail.com>
2020-04-21 20:20:43 -0300

    feat: add User router resources url in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import postController from './controllers/postController'
 import tagController from './controllers/tagController'
 import categoryController from './controllers/categoryController'
+import userController from './controllers/userController'
 import 'reflect-metadata'
 import './database'
 const app = express()
@@ -13,5 +14,6 @@ app.use(cors())
 app.use('/posts', postController)
 app.use('/tags', tagController)
 app.use('/categories', categoryController)
+app.use('/users', userController)
 
 app.listen(8000, () => console.log('app server running : http://localhost:8000'))
