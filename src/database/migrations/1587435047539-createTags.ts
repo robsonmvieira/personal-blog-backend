ab5cd67a1f0d2113f3a6f85d8ec20dcf12e0f028
Robson Maia <robsonmvieira@gmail.com>
2020-04-20 23:16:17 -0300

    feat: add Tag migration

---
+++ src/database/migrations/1587435047539-createTags.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createTags1587435047539 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
