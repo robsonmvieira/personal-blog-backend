9a0d3ad4bc0b6fc836694c581e0d6eb123565549
Robson Maia <robsonmvieira@gmail.com>
2020-04-21 20:01:44 -0300

    feat: define  url paths to post resource

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -2,8 +2,26 @@ import { Router } from 'express'
 
 const routes = Router()
 
+routes.post('/', (req, res) => {
+  res.status(200).json({ ...req.body })
+})
+
 routes.get('/', (req, res) => {
   res.status(200).json({ msg: 'ok' })
 })
 
+routes.get('/:id', (req, res) => {
+  const { id } = req.params
+  res.status(200).json({ msg: id })
+})
+routes.put('/:id', (req, res) => {
+  const { title } = req.body
+  const { id } = req.params
+  res.status(200).json({ msg: id, body: title })
+})
+
+routes.delete('/:id', (req, res) => {
+  const { id } = req.params
+  res.status(200).json({ msg: id, body: 'ok' })
+})
 export default routes
