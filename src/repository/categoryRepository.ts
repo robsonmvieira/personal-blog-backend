9eaacebb26c4df317437a3660325040f19d1317a
Robson Maia <robsonmvieira@gmail.com>
2020-04-27 23:13:12 -0300

    feat: create categoryRepository

---
+++ src/repository/categoryRepository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from 'typeorm'
+
+import { Category } from '../models/category'
+
+@EntityRepository(Category)
+export class CategoryRepository extends Repository<Category> {
+
+}
