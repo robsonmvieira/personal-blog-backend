8584e04a0a8ecb7b44d1e5bddfb5c5b08e3a81a9
Robson Maia <robsonmvieira@gmail.com>
2020-04-27 23:16:44 -0300

    feat: add User schema design in migration

--- src/database/migrations/1587435072087-createUsers.ts
+++ src/database/migrations/1587435072087-createUsers.ts
@@ -1,11 +1,53 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class createUsers1587435072087 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+  public async down (queryRunner: QueryRunner): Promise<void> {
+  }
 }
