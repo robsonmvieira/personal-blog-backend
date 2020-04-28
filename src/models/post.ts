f5d41e2bc0ef5e747d04d0398e0c3543410a451b
Robson Maia <robsonmvieira@gmail.com>
2020-04-28 16:12:09 -0300

    feat: add relationship between  more than 1 model

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,7 +1,10 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, ManyToOne } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, ManyToOne, ManyToMany } from 'typeorm'
 import Category from './category'
+import User from './user'
+import Tag from './tag'
+import Comment from './comments'
 
-@Entity()
+@Entity('posts')
 export default class Post {
   @PrimaryGeneratedColumn('uuid')
   id: string;
@@ -18,12 +21,27 @@ export default class Post {
   @Column()
   content: string
 
+  @Column({ type: 'uuid' })
+  category_id: string
+
   @ManyToOne(type => Category, category => category.posts)
   category: Category
+
+  @ManyToMany(type => Tag, tag => tag.posts)
+  tags: Tag[]
+
+  @ManyToOne(type => User, user => user.posts)
+  user: User
+
+  @OneToMany(type => Comment, comment => comment.post)
+  comments: Comment[]
 
   @CreateDateColumn()
   cretated_at: Date
 
   @UpdateDateColumn()
   updated_at: Date
+
+  @Column({ type: 'uuid' })
+  user_id: string
 }
