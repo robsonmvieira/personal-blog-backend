ea500b9c93566addb9cbb9d4f8eb06200181d0a6
Robson Maia <robsonmvieira@gmail.com>
2020-04-28 11:53:23 -0300

    feat: update way to import Cat from repository

--- src/repository/categoryRepository.ts
+++ src/repository/categoryRepository.ts
@@ -1,6 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm'
-
-import { Category } from '../models/category'
+import Category from '../models/category'
 
 @EntityRepository(Category)
 export class CategoryRepository extends Repository<Category> {
