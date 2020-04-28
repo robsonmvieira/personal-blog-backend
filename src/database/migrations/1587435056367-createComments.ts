c202ed80bc328bf95f31ec84070fa66ddeda52fd
Robson Maia <robsonmvieira@gmail.com>
2020-04-27 23:16:34 -0300

    feat: add Comments schema design in migration

--- src/database/migrations/1587435056367-createComments.ts
+++ src/database/migrations/1587435056367-createComments.ts
@@ -1,11 +1,49 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
 
 export class createComments1587435056367 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'comments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'content',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()'
+          }
+        ]
+      })
+    )
+    await queryRunner.createForeignKey('comments',
+      new TableForeignKey({
+        name: 'commentPost',
+        columnNames: ['id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'posts',
+        onDelete: 'CASCADE'
+      }))
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<any> {
-    }
-
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('comments', 'commentPost')
+    await queryRunner.dropTable('comments')
+  }
 }
