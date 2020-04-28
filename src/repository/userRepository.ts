753e94ee269d35b1de3c338aec7b44ce2e4d64c4
Robson Maia <robsonmvieira@gmail.com>
2020-04-28 16:13:41 -0300

    feat: create UserRepository

---
+++ src/repository/userRepository.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from 'typeorm'
+import User from '../models/user'
+
+@EntityRepository(User)
+export default class userRepository extends Repository<User> {
+
+}
