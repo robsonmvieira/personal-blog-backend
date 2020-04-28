68ca3b3536c28bb64546a91fa80eb754a4315e2a
Robson Maia <robsonmvieira@gmail.com>
2020-04-28 16:12:55 -0300

    feat: add relationship between User and Post

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,8 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
+import Post from './post'
 
-export class User {
+@Entity('users')
+export default class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column()
   name: string
+
+  @Column()
   email: string
+
+  @Column()
   password: string
+
+  @Column()
   isAdmin: string
+
+  @OneToMany(type => Post, post => post.user)
+  posts: Post[]
 }
