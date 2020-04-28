c00e4692366be84b7a6df16dad8fb79188fe63df
Robson Maia <robsonmvieira@gmail.com>
2020-04-28 16:11:13 -0300

    feat: add relationship between comments and posts

--- src/models/comments.ts
+++ src/models/comments.ts
@@ -1,7 +1,18 @@
-import { Post } from './post'
 
-export class Comment {
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm'
+import Post from './post'
+
+@Entity('comments')
+export default class Comment {
+  @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column()
+  post_id: string
+
+  @ManyToOne(type => Post, post => post.comments)
   post: Post
+
+  @Column()
   content: string
 }
