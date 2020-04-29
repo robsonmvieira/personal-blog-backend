b1b01146b6940553f874439efabcf71081a51613
Robson Maia <robsonmvieira@gmail.com>
2020-04-29 20:31:57 -0300

    add connect between Db and tag controller

--- src/controllers/tagController.ts
+++ src/controllers/tagController.ts
@@ -1,30 +1,45 @@
+import { getCustomRepository } from 'typeorm'
 import { Router } from 'express'
+import TagRepository from '../repository/tagRepository'
 
 const routes = Router()
 
-routes.post('/', (req, res) => {
-  const { title } = req.body
-  res.status(200).json({ msg: title })
+routes.post('/', async (req, res) => {
+  const tagRepository = getCustomRepository(TagRepository)
+  const tag = await tagRepository.save(req.body)
+  res.status(200).json(tag)
 })
 
-routes.get('/', (req, res) => {
-  res.status(200).json({ msg: 'ok' })
+routes.get('/', async (req, res) => {
+  const tagRepository = getCustomRepository(TagRepository)
+  const index = await tagRepository.find()
+  res.status(200).json(index)
 })
 
-routes.get('/:id', (req, res) => {
+routes.get('/:id', async (req, res) => {
   const { id } = req.params
-  res.status(200).json({ msg: id })
+  const tagRepository = getCustomRepository(TagRepository)
+  const index = await tagRepository.findOne({ id })
+  res.status(200).json(index)
 })
 
-routes.put('/:id', (req, res) => {
-  const { title } = req.body
+routes.put('/:id', async (req, res) => {
   const { id } = req.params
-  res.status(200).json({ msg: id, body: title })
+  const tagRepository = getCustomRepository(TagRepository)
+  const tag = await tagRepository.update(id, req.body)
+  res.status(200).json(tag)
 })
 
-routes.delete('/:id', (req, res) => {
+routes.delete('/:id', async (req, res) => {
   const { id } = req.params
-  res.status(200).json({ msg: id, body: 'ok' })
+  const tagRepository = getCustomRepository(TagRepository)
+  const index = await tagRepository.findOne({ id })
+
+  if (index != null) {
+    await tagRepository.remove(index)
+    return res.status(200).json(true)
+  }
+  return res.status(401).json({ message: 'tag was not found' })
 })
 
 export default routes
