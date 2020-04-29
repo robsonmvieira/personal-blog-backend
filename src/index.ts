d7b110c0994f50c3d2e449d2357b8ea4b2abd8d7
Robson Maia <robsonmvieira@gmail.com>
2020-04-29 20:02:31 -0300

    feat: add comments router to index

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import postController from './controllers/postController'
 import tagController from './controllers/tagController'
 import categoryController from './controllers/categoryController'
 import userController from './controllers/userController'
-
+import commentController from './controllers/commentController'
 import './database'
 
 const app = express()
@@ -17,5 +17,6 @@ app.use('/posts', postController)
 app.use('/tags', tagController)
 app.use('/categories', categoryController)
 app.use('/users', userController)
+app.use('/comments', commentController)
 
 app.listen(8000, () => console.log('app server running : http://localhost:8000'))
