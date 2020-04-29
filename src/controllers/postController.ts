18051d03961c969fb0ad4122125d527e8df6126e
Robson Maia <robsonmvieira@gmail.com>
2020-04-29 19:15:13 -0300

    feat: fiix remove mothod from postController

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -20,18 +20,33 @@ routes.get('/', async (req, res) => {
   res.status(200).json(posts)
 })
 
-routes.get('/:id', (req, res) => {
+routes.get('/:id', async (req, res) => {
   const { id } = req.params
-  res.status(200).json({ msg: id })
+  const postRepository = getCustomRepository(PostRepository)
+  const post = await postRepository.findOne({ id })
+  res.status(200).json(post)
 })
-routes.put('/:id', (req, res) => {
-  const { title } = req.body
+routes.put('/:id', async (req, res) => {
   const { id } = req.params
-  res.status(200).json({ msg: id, body: title })
+  const postRepository = getCustomRepository(PostRepository)
+  const post = await postRepository.update(id, req.body)
+  res.status(200).json(post)
 })
 
-routes.delete('/:id', (req, res) => {
-  const { id } = req.params
-  res.status(200).json({ msg: id, body: 'ok' })
+routes.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const postRepository = getCustomRepository(PostRepository)
+    const post = await postRepository.findOne({ id })
+    // console.log(post)
+    if (post != null) {
+      const r = await postRepository.remove(post)
+      console.log(r)
+      return res.status(200).json(true)
+    }
+    return res.status(401).json({ msg: 'Post was not found' })
+  } catch (error) {
+    console.log(error)
+  }
 })
 export default routes
