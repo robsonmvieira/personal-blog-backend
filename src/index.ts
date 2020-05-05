62cc7c53e089fb845619239928811d40b6a3694b
Robson Maia <robsonmvieira@gmail.com>
2020-05-05 18:35:54 -0300

    chore: update config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
+import { container } from 'tsyringe'
+// import CategoryRepository from '@infra/repository/categoryRepository'
 import postController from './controllers/postController'
 import tagController from './controllers/tagController'
-import categoryController from './controllers/categoryController'
+// import categoryController from './controllers/categoryController'
 import userController from './controllers/userController'
 import commentController from './controllers/commentController'
 import sessionController from './controllers/sessionController'
 import '@infra/database'
-
+import '@infra/container'
+import CategoryController from 'controllers/categoryController'
+const categoryController = container.resolve(CategoryController)
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-
 app.use('/posts', postController)
 app.use('/tags', tagController)
-app.use('/categories', categoryController)
+app.get('/categories', categoryController.index)
 app.use('/users', userController)
 app.use('/comments', commentController)
 app.use('/sessions', sessionController)
