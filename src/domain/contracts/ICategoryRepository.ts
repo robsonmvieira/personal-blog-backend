9f5a192cbd05e10918a4dcc35b8a6e6d82cd8c02
Robson Maia <robsonmvieira@gmail.com>
2020-05-05 15:27:33 -0300

    feat: update ICateoryRepository

--- src/domain/contracts/ICategoryRepository.ts
+++ src/domain/contracts/ICategoryRepository.ts
@@ -4,6 +4,6 @@ export default interface ICategoryRepository {
   save(category: Category): Promise<Category>
   index(): Promise<Category[]>
   show(id: string): Promise<Category | undefined>
-  update(id: string, category: Category): Promise<Category | undefined>
+  update(id: string, category: Category): Promise<Category>
   remove(id: string): Promise<boolean>
 }
