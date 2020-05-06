942bd749bf59f21603ca95498f7ffcc913cc68b7
Robson Maia <robsonmvieira@gmail.com>
2020-05-06 20:07:57 -0300

    feat: update Index startup file

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
 import 'reflect-metadata'
 import express from 'express'
 import cors from 'cors'
-import { container } from 'tsyringe'
-// import CategoryRepository from '@infra/repository/categoryRepository'
+import '@infra/database'
+import '@infra/container'
 import postController from './controllers/postController'
 import tagController from './controllers/tagController'
-// import categoryController from './controllers/categoryController'
 import userController from './controllers/userController'
 import commentController from './controllers/commentController'
 import sessionController from './controllers/sessionController'
-import '@infra/database'
-import '@infra/container'
-import CategoryController from 'controllers/categoryController'
-const categoryController = container.resolve(CategoryController)
+import categoryRouter from '@infra/routes/categoryRoutes'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use('/posts', postController)
 app.use('/tags', tagController)
-app.get('/categories', categoryController.index)
+app.use('/categories', categoryRouter)
+
 app.use('/users', userController)
 app.use('/comments', commentController)
 app.use('/sessions', sessionController)
