000f8f866f8f3fb60209d9299bfad64b5add8cdf
Robson Maia <robsonmvieira@gmail.com>
2020-05-06 20:05:24 -0300

    feat: add UserInterface to container resolver

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -1,7 +1,13 @@
+import UserRepository from '@infra/repository/userRepository'
 import { container } from 'tsyringe'
 
 import ICategoryRepository from '@domain/contracts/ICategoryRepository'
 import CategoryRepository from '@infra/repository/categoryRepository'
 
+import '@infra/adapters'
+import IUserRepository from '@domain/contracts/IUserRepository'
+
 container.registerSingleton<ICategoryRepository>(
   'CategoryRepository', CategoryRepository)
+
+container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
