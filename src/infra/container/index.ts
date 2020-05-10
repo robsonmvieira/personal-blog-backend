2776612fe31da971e4d12bd2c2132e6cf863ec81
Robson Maia <robsonmvieira@gmail.com>
2020-05-09 23:53:43 -0300

    feat: registration ITagRepository to container

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -1,13 +1,16 @@
-import UserRepository from '@infra/repository/userRepository'
 import { container } from 'tsyringe'
 
-import ICategoryRepository from '@domain/contracts/ICategoryRepository'
-import CategoryRepository from '@infra/repository/categoryRepository'
-
 import '@infra/adapters'
+import ICategoryRepository from '@domain/contracts/ICategoryRepository'
+import CategoryRepository from '@infra/repositories/categoryRepository'
 import IUserRepository from '@domain/contracts/IUserRepository'
+import ITagRepository from '@domain/contracts/ITagRepository'
+import UserRepository from '@infra/repositories/userRepository'
+import TagRepository from '@infra/repositories/tagRepository'
 
 container.registerSingleton<ICategoryRepository>(
   'CategoryRepository', CategoryRepository)
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+
+container.registerSingleton<ITagRepository>('TagRepository', TagRepository)
